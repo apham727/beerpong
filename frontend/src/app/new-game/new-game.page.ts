baeb62bfdba831c4d3d3f138fa883ab533ab56f7
Andrew Pham <apham727@gmail.com>
2019-10-20 16:17:52 -0500

    attempting to fix cors

--- frontend/src/app/new-game/new-game.page.ts
+++ frontend/src/app/new-game/new-game.page.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule, Injectable } from '@angular/core';
 import { Router } from "@angular/router";
-
+// import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-game',
   templateUrl: './new-game.page.html',
   styleUrls: ['./new-game.page.scss'],
 })
+
+@Injectable()
 export class NewGamePage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   register(form) {
+    console.log("about to call endpoint");
+    var serverResp = this.http.get("https://localhost:8000/").subscribe(response => console.log(response.toString()));
     this.router.navigateByUrl('home');
   }
 
