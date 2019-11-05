ab4de2109f5b20d98b278a967312b1292449b342
Andrew Pham <apham727@gmail.com>
2019-11-04 18:55:50 -0600

    tried making post

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,19 @@ app.get("/", (req, res) => {
     res.status(200).send("suk my pepeasef");
 });
 
+app.put("/new_game", (req, res) => {
+    
+});
+
+app.post("/create_new_game", (req, res) => {
+    console.log("request is");
+    console.log(req);
+    res.status(200).send("suk my pepe 2");
+})
+
+
 app.route("/existing_games").get((req, res) => {
-    res.status(200).send("suk my pepe from node");
+    res.status(200).send("suk my pepe");
 });
 
 app.listen(port, () => {
--- frontend/src/app/new-game/new-game.page.ts
+++ frontend/src/app/new-game/new-game.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, NgModule, Injectable } from '@angular/core';
 import { Router } from "@angular/router";
-// import { Observable } from 'rxjs/Observable';
+import { Location } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { ServerResponse } from 'http';
 
 @Component({
   selector: 'app-new-game',
@@ -12,14 +13,31 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class NewGamePage implements OnInit {
 
-  constructor(private router: Router, private http: HttpClient) { }
+  private lng: number;
+  private lat: number;
+
+  constructor(private router: Router, private http: HttpClient, private location: Location) { 
+    this.lng = -95.398829;
+    this.lat = 29.715297;
+  }
 
   ngOnInit() {
+    if (navigator.geolocation) {
+      console.log("Geolocation service available in user browser");
+      navigator.geolocation.getCurrentPosition(currentPosition => {
+        console.log('currentPosition', currentPosition);
+        this.lng = currentPosition.coords.longitude;
+        this.lat = currentPosition.coords.latitude;
+      })
+    } else {
+      console.log("geolocation service not available;");
+    }
   }
 
   register(form) {
-    console.log("about to call endpointt");
+    console.log(form.value)
     var serverResp = this.http.get("/api/existing_games", { responseType: 'text' }).subscribe(response => console.log(response.toString()));
+    var postResp = this.http.post("/api/create_new_game", form.value, { responseType: 'text' }).subscribe(response => console.log(response.toString()));
     this.router.navigateByUrl('home');
   }
 
