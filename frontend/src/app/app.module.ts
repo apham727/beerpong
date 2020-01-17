23cfa8b6b70bbfc1f911588c7d227e3bf97c6568
apham727 <apham727@gmail.com>
2020-01-17 16:33:45 -0600

    popup box

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +19,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule, 
-    HttpClientModule
+    HttpClientModule,
+    MatDialogModule,
+    BrowserAnimationsModule, 
   ],
   providers: [
     StatusBar,
--- frontend/src/app/list/list.page.ts
+++ frontend/src/app/list/list.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from "@angular/router";
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -12,7 +13,7 @@ export class ListPage implements OnInit {
   public items: Array<{ title: string; queue: Array<String>}> = [];
 
 
-  constructor(private router: Router, private http: HttpClient) {
+  constructor(private router: Router, private http: HttpClient, private alertCtrl: AlertController) {
 
       this.http.get("http://localhost:8000/existing_games",{ responseType: 'text' }).subscribe(resp => {
 
@@ -35,4 +36,38 @@ export class ListPage implements OnInit {
   // navigate(item) {
   //   this.router.navigate(['/list', JSON.stringify(item)]);
   // }
+
+
+  openDialog(gameToJoin) {
+    let alert = this.alertCtrl.create({
+      inputs: [
+        {
+          name: 'username',
+          placeholder: 'Username'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Join',
+          handler: data => {
+            var joinData = {
+              gameName: gameToJoin,
+              playerName: data.username
+            }
+            console.log(joinData);
+            var postResp = this.http.post("http://localhost:8000/join_existing_game", joinData, { responseType: 'text' }).subscribe(response => console.log(response.toString()));
+
+          }
+        }
+      ]
+    });
+    alert.then(al => al.present());
+  }
 }
